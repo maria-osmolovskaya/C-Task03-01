b3a3acf70f97ab46ff55a25c716edd3e4a07bc02
Maria Osmolovskaya <maria.osmolovskaya@gmail.com>
2022-12-12 01:20:20 +0300

    Программа для проверки пятизначного числа - Паллиндром или нет

---
+++ HomeTask03-01/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+*/
+
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result.ToString().Length!>4 && result.ToString().Length<6)
+        {
+            isCorrect = true;
+        }
+        else 
+        {
+            Console.WriteLine("Вы ввели не пятизначное число. Введите пятизначное число");
+        }
+    }
+    return result;
+}
+
+int Number = GetNumber ("Введите пятизначное число");
+
+int FirstDigit = Number / 10000;
+int SecondDigit = ((Number / 1000) % 10);
+int FourthDigit = ((Number % 100) / 10);
+int FifthDigit = Number % 10;
+
+if (FirstDigit==FifthDigit && SecondDigit==FourthDigit)
+{
+Console.WriteLine ("да");
+}
+else
+Console.WriteLine ("нет");
---
+++ Task01/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.WriteLine ("Введите первое число");
+string numberOneStr = Console.ReadLine ();
+
+Console.WriteLine ("Введите второе число");
+string numberTwoStr = Console.ReadLine ();
+
+int NumberOne = Convert.ToInt32(numberOneStr);
+int NumberTwo = Convert.ToInt32(numberTwoStr);
+
+if (NumberOne > NumberTwo)
+{
+    Console.WriteLine("Число 1 больше Числа 2");
+}
+else
+{
+    Console.WriteLine("Число 2 больше Числа 1");
+}
